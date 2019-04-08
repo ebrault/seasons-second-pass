9df7983a4daec0808b0d0ce459310be7ee20ebb1
ebrault <emmet.brault@gmail.com>
2019-04-08 19:17:37 -0400

    Added SeasonConfig Variable
    Added SeasonConfig variable
    
    Rather than having to manage variable logic in ternary expressions that
    evaluate the same things, we have created a config variable called
    SeasonConfig, that is an Object containing keys assigned to seasons and
    values assigned to objects containing key/value pairs for text and icon.
    In our component, we destructure out text and icon as text and iconName
    and assign them to seasonConfig[season]. We then change our template
    strings to iconName to reflect this destructure
    
    Changes verified as app still works

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,22 +1,32 @@
 import React from 'react'
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Brr it's chilly!",
+    iconName: "snowflake"
+  }
+}
+
 const getSeason = (lat, month) => {
  if (month > 2 && month < 9) {
-   return lat > 0 ? 'Summer' : 'Winter'
+   return lat > 0 ? 'summer' : 'winter'
  } else {
-   return lat < 0 ? 'Winter' : 'Summer'
+   return lat < 0 ? 'winter' : 'summer'
  }
 }
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth())
-  const text = season === 'Winter' ? 'Brr it is chilly' : 'Lets hit the beach'
-  const icon = season === 'Winter' ? 'snowflake' : 'sun'
+  const {text, iconName} = seasonConfig[season]
   return (
     <div>
-      <i className={`icon ${icon}`} />
+      <i className={`icon ${iconName}`} />
       <h1>{text}</h1>
-      <i className={`icon ${icon}`} />
+      <i className={`icon ${iconName}`} />
     </div>
   )
 }
