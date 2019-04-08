f5d0218f5a1286d9f6c009d01a5548ea9d6fc573
ebrault <emmet.brault@gmail.com>
2019-04-08 19:10:05 -0400

    Added Icons
    Added icons
    - Looked at Semantic UI documentation to figure out how to implement
      icon tags and also to see what icons are available to us
      - Looking at mockups, we want to implement snowflake icons when the
        season is winter, and sun icons when the season is summer
      - Semantic UI thankfully has both of these icons available
    - Added a variable icon and assigned it to a ternary expression checking
      whether the season is summer or winter and rendering the appropriate
      icon conditionally
    - Wrapped the text h1 in two icon tags with a className equal to a
      template string, the icon tag, and the icon name as a template that
      will change conditionally
    - Noticed that this reference to icon as a variable since it is also
      a Semantic UI tag is confusing and that our ternary expression
      for icon and season is almost identical, so we will refactor

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,9 +11,12 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth())
   const text = season === 'Winter' ? 'Brr it is chilly' : 'Lets hit the beach'
+  const icon = season === 'Winter' ? 'snowflake' : 'sun'
   return (
     <div>
+      <i className={`icon ${icon}`} />
       <h1>{text}</h1>
+      <i className={`icon ${icon}`} />
     </div>
   )
 }
