a5035afcbcd82fd814cba34627cbec8d45201b6e
ebrault <emmet.brault@gmail.com>
2019-04-08 18:28:15 -0400

    Imported SeasonDisplay Component
    Imported SeasonDisplay component to index.js
    
    - We do not want to render the latitude to the user when they load up
      the page; the goal of this app is to tell the user what season it is
      based on their latitude. In place of the passing of this.state.lat as
      a variable to the wrapping div in a return, we show an instance of
      the SeasonDisplay component. This is verified by refreshing the page
      and seeing the content of our SeasonDisplay component rendered.
    - We can also pass state as props from parent to child components.
      - Giving the instance of the SeasonDisplay component a prop of lat
        we can assign this prop the variable this.state.lat
      - This means that when we call this.setState in order to update the
        latitude in our parent component, latitude in SeasonDisplay will
        update / rerender as well
    - When this.setState is called, it will render changes to the parent
      component, but it will also render changes to any child components
      with state passed as props as well.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
   state = { lat: null, errorMessage: '' }
-  
+
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
@@ -19,7 +20,9 @@ class App extends React.Component {
     }
     if (!this.state.errorMessage && this.state.lat) {
       return (
-        <div>Latitude: {this.state.lat}</div>
+        <div>
+          <SeasonDisplay lat={this.state.lat}/>
+        </div>
       )
     }
     return <div>Loading...</div>
