f17dcb461f8ddc18183a8d9d0f6e4c746302b292
ebrault <emmet.brault@gmail.com>
2019-04-08 19:33:47 -0400

    Added Loading Spinner
    Added loading spinner
     - When our page renders, if the user has not accepted the location
       request, they are presented with a white page that says "Loading..."
       in black text. This is something we want to avoid
     - Looked at Semantic UI documentation for loaders and picked one
     - Created a Spinner component, functional component that renders a
       spinning loader picked from Semantic UI with text that we will
       customize. For now the text is going to be "Loading..."
     - Imported Spinner into index.js
     - In place of our "Loading..." render, we are going to render an
       instance of our Spinner component
     - Tested successfully: turned off location access, refreshed, and
       saw spinner until location request is accepted or denied

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
 class App extends React.Component {
   state = { lat: null, errorMessage: '' }
@@ -25,7 +26,11 @@ class App extends React.Component {
         </div>
       )
     }
-    return <div>Loading...</div>
+    return (
+      <div>
+        <Spinner/>
+      </div>
+    )
   }
 }
 
